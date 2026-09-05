5d26be46218bae18f4781975ea31309de872de3f
theisaacsaq <theisaacsaq@gmail.com>
2026-09-06 02:43:37 +0500

    Add files via upload

---
+++ client-data.js
@@ -0,0 +1,48 @@
+/* =========================================================
+   CLIENT DATA
+   Har naya order aaye to SIRF is file ko edit karein.
+========================================================= */
+
+const CLIENT = {
+  bride: "جبران",
+  groom: "علی",
+  father: "چوہدری احمد",
+  host: "جناب و محترمہ __________",
+  whatsapp: "923001234567",
+  maxGuests: 4,
+
+  barat: {
+    enabled: true,
+    dateText: "بروز ہفتہ، ۱۲ دسمبر ۲۰۲۶",
+    timeText: "شام ۷:۰۰ بجے",
+    venue: "باراتی ہال، کراچی",
+    mapQuery: "Barati Hall Karachi Pakistan",
+    start: "20261212T140000Z",
+    end: "20261212T180000Z"
+  },
+
+  walima: {
+    enabled: true,
+    dateText: "بروز اتوار، ۱۳ دسمبر ۲۰۲۶",
+    timeText: "رات ۸:۰۰ بجے",
+    venue: "پرل بینکوئٹ، کراچی",
+    mapQuery: "Pearl Banquet Karachi Pakistan",
+    start: "20261213T150000Z",
+    end: "20261213T190000Z"
+  },
+
+  countdownTo: "2026-12-12T19:00:00+05:00",
+
+  elders: [
+    "جناب محمد علی صاحب",
+    "جناب احمد علی صاحب",
+    "جناب عثمان علی صاحب"
+  ],
+
+  cousins: [
+    "احمد",
+    "علی",
+    "حسن",
+    "حسین"
+  ]
+};
